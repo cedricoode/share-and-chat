7143518651a9230a58edb8a822229c1bb5ecae3a
Elegode <cedric.yang@ihealthlabs.eu>
2017-11-25 16:52:01 +0100

    hotfix - readd login commonent

--- src/navComponents.js
+++ src/navComponents.js
@@ -2,13 +2,9 @@ import { Navigation } from 'react-native-navigation';
 
 import MenuButton from './components/menuButtonComponent';
 import SideMenu from './containers/sideMenuContainer';
-import LogoutButton from './components/logoutButton';
 import { components } from '../config/constants';
-
-import store from '../store';
 
 export default function registerComponents() {
     Navigation.registerComponent(components.menuButton, () => MenuButton);
     Navigation.registerComponent(components.sideMenu, () => SideMenu);
-    Navigation.registerComponent(components.logoutButton, () => LogoutButton, store);
 }
